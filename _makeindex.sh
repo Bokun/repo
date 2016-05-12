7704bc60eb88d3730307daea6b0991f343ffcb8f
Sindri Traustason <sindri@bokun.is>
2016-05-12 14:14:00 +0000

    generate in given folder

--- _makeindex.sh
+++ _makeindex.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 start=`pwd`
-for directory in `find . -type d`
+for directory in `find $1 -type d`
 do
   cd ${directory} && ls | perl -e 'print "<html><body><ul>"; while(<>) { chop $_;  print "<li><a href=\"./$_\">$_</a></li>";} print "</ul></body></html>"' > index.html
   cd ${start}
