529136934805bafaf4e408973dbe072c7b0b3e1a
Sindri Traustason <sindri@bokun.is>
2016-06-20 14:33:01 +0000

    Don't show index.html files in index.html added .gitignore

--- _makeindex.sh
+++ _makeindex.sh
@@ -2,6 +2,6 @@
 start=`pwd`
 for directory in `find $1 -type d`
 do
-  cd ${directory} && ls | perl -e 'print "<html><body><ul>"; while(<>) { chop $_;  print "<li><a href=\"./$_\">$_</a></li>";} print "</ul></body></html>"' > index.html
+  cd ${directory} && ls | grep -v index.html | perl -e 'print "<html><body><ul>"; while(<>) { chop $_;  print "<li><a href=\"./$_\">$_</a></li>";} print "</ul></body></html>"' > index.html
   cd ${start}
 done
